69cd4e0a198ca7c8bb9c1fbee4db6c1c1abd9d1c
manrandomside <cussrombymangans1@gmail.com>
2026-01-01 15:15:09 +0700

    #1 Testing Hosting

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ticketing System UPT LAB</title>
+    <title>Layanan Kebutuhan Akademisi - Citra Konsultama Indonesia</title>
+    <link rel="icon" type="image/png" href="/images/logo_web_UPT.png">
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
